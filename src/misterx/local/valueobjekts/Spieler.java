8d34ef9bf637a7a19524764c01e7ffdb024bd01a
gigi001 <emser__hatice@hotmail.de>
2016-04-20 11:34:10 +0200

    exception

--- src/misterx/local/valueobjekts/Spieler.java
+++ src/misterx/local/valueobjekts/Spieler.java
@@ -1,15 +1,61 @@
 package misterx.local.valueobjekts;
 
+import java.util.ArrayList;
+
 public class Spieler {
 
 	private String name;
 	private int taxiChips;
-
+	private int busChips;
+	private int bahnChips;
+    private ArrayList <Integer> guthaben;
+	
 	public Spieler(String name) {
 		this.name = name;
+		guthaben.add(taxiChips,30);
+		guthaben.add(busChips,40);
+		guthaben.add(bahnChips, 50);
+	}
+
+	public void setTaxiChips(int taxiChips) {
+		this.taxiChips = taxiChips;
+	}
+
+	public void setBusChips(int busChips) {
+		this.busChips = busChips;
+	}
+
+	public void setBahnChips(int bahnChips) {
+		this.bahnChips = bahnChips;
 	}
 
 	public String getName() {
 		return name;
 	}
-}
+	
+	public int getTaxiChips(){
+		return taxiChips;
+	}
+	
+	public int getBusChips(){
+		return busChips;
+	}
+	
+	public int getBahnChips(){
+		return bahnChips;
+	}
+	
+	
+		
+		
+		
+	}
+	
+	
+	
+	}
+	
+	
+	
+	
+
