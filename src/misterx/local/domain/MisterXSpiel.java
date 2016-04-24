b2edb12fccd29d2fef1e737b23707a38ea6de49a
D3LV4R <chrisalbers.96@gmail.com>
2016-04-24 13:19:38 +0200

    042404

--- src/misterx/local/domain/MisterXSpiel.java
+++ src/misterx/local/domain/MisterXSpiel.java
@@ -1,5 +1,6 @@
 package misterx.local.domain;
 
+import java.util.Iterator;
 import java.util.List;
 
 import misterx.local.domain.exceptions.SpielerExistiertBereitsException;
@@ -10,6 +11,7 @@ public class MisterXSpiel {
 
 	private StadtVerw sv = new StadtVerw();
 	private SpielerVerw spielerVW = new SpielerVerw();
+	private Spiellogik logik = new Spiellogik();
 		
 	public void spielerHinzufügen(Spieler neuerspieler) throws SpielerExistiertBereitsException { 
 		 spielerVW.spielerHinzufügen(neuerspieler);
@@ -31,5 +33,56 @@ public class MisterXSpiel {
 	public Station getStationByIndex(int index){
 		return sv.getStationByIndex(index);
 	}
+	
+	
+	public int getRunde() {
+		return logik.getRunde();
+	}
+	
+	public int getNaechsteRunde() {
+		return logik.getNaechsteRunde();
+	}
+	
+	
+	public int getGewonnen() {
+		return logik.getGewonnen();
+	}
+	
+	public int getXWin(Spieler spieler, Spieler misterx) {
+		return logik.getXWin(spieler, misterx);
+	}
+	
+	
+	public boolean getZeigen() {
+		return logik.getZeigen();
+	}
+
+	
+	public void taxiAbziehen(Spieler spieler, Spieler misterx){
+		logik.taxiAbziehen(spieler, misterx);
+	}
+	
+	public void busAbziehen(Spieler spieler, Spieler misterx){
+		logik.busAbziehen(spieler, misterx);
+	}
+	
+	public void bahnAbziehen(Spieler spieler, Spieler misterx){
+		logik.bahnAbziehen(spieler, misterx);
+	}
+	
+	
+	public void taxiFahren(Iterator<Station> nachbTaxiIterator, String stationsausw, Spieler spieler, Spieler misterx, int l) {
+		logik.taxiFahren(nachbTaxiIterator, stationsausw, spieler, misterx, l);
+	}
+	
+	public void busFahren(Iterator<Station> nachbBusIterator, String stationsausw, Spieler spieler, Spieler misterx, int l) {
+		logik.busFahren(nachbBusIterator, stationsausw, spieler, misterx, l);
+	}
+	
+	public void bahnFahren(Iterator<Station> nachbBahnIterator, String stationsausw, Spieler spieler, Spieler misterx, int l) {
+		logik.bahnFahren(nachbBahnIterator, stationsausw, spieler, misterx, l);
+	}
+	
+	
 }
 
