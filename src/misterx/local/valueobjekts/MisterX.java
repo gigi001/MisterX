b8d67cf00a4c1fa875e33bc1e2b597ae9f3e549b
gigi001 <emser__hatice@hotmail.de>
2016-05-25 12:19:53 +0200

    0525

--- src/misterx/local/valueobjekts/MisterX.java
+++ src/misterx/local/valueobjekts/MisterX.java
@@ -2,6 +2,10 @@ package misterx.local.valueobjekts;
 
 public class MisterX extends Spieler  {
 
+	protected int blackTickets;
+	protected int doubleChips;
+	
+	
 	/**
 	 * 
 	 */
@@ -12,7 +16,30 @@ public class MisterX extends Spieler  {
 		blackTickets = 5;
 		doubleChips = 2;
 	}
+	public MisterX(String name,int blackTickets, int doubleChips, int taxiChips, int bahnChips, int busChips) {
+		super(name,taxiChips,busChips, bahnChips);
+		this.blackTickets = blackTickets;
+		this.doubleChips = doubleChips;
 	
+	}
+	
+	
+	public void setBlackTickets(int blackTickets){
+		this.blackTickets = blackTickets;
+	}
+	
+	public int getBlackTickets(){
+		return blackTickets;
+	}
+	
+	
+	public void setDoubleChips(int doubleChips){
+		this.doubleChips = doubleChips;
+	}
+	
+	public int getDoubleChips(){
+		return doubleChips;
+	}
 	
 
 }
